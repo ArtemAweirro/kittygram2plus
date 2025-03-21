10582b70a136d564f7b48164cd4eed3bf6f7126d
Artem Sikhvart <sixvart05@bk.ru>
2025-03-21 20:31:48 +0600

    Использование разных permissions в зависимости от ситуации

--- cats/views.py
+++ cats/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from .permissions import OwnerOrReadOnly
+from .permissions import OwnerOrReadOnly, ReadOnly
 
 from .models import Achievement, Cat, User
 
@@ -13,6 +13,14 @@ class CatViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+    
+    def get_permissions(self):
+        # Если в GET-запросе требуется получить информацию об объекте
+        if self.action == 'retrieve':
+            # Вернём обновлённый перечень используемых пермишенов
+            return (ReadOnly(),)
+        # Для остальных ситуаций оставим текущий перечень пермишенов без изменений
+        return super().get_permissions() 
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
